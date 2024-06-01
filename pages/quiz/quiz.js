f8ddeffc99dacc8bf162d5815da7abc28c37d63a
Arthur Menocci <arthur.menocci@eaportal.org>
2024-06-01 02:14:39 +0000

    adicionada a função de enviar a resposta

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -6,9 +6,10 @@ const assunto = localStorage.getItem("assunto")
 
 let quiz = {}
 let pontos = 0
-let pergunta = 2
+let pergunta = 1
 let resposta = ""
 let idInputResposta = ""
+let respostaCorretaId = ""
 
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
@@ -105,7 +106,20 @@ async function buscarPerguntas () {
     function guardarResposta(evento) {
         resposta = evento.target.value
         idInputResposta = evento.target.id
+
+        const botaoEnviar = document.querySelector(".alternativas button")
+        botaoEnviar.addEventListener("click", validarResposta)
     }
+
+        function validarResposta() {
+            if (resposta === quiz.questions[pergunta-1].answer) {
+                document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "correta")
+                pontos = pontos + 1
+            } else {
+                document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "errada")
+                document.querySelector(`label[for='${respostaCorretaId}']`).setAttribute("id", "correta")
+            }
+        }
 
     async function iniciar() {
         alterarAssunto()
@@ -115,6 +129,10 @@ async function buscarPerguntas () {
         const inputsResposta = document.querySelectorAll(".alternativas input") 
         inputsResposta.forEach (input => {
             input.addEventListener("click", guardarResposta)
+
+            if (input.value === quiz.questions[pergunta-1].answer) {
+                respostaCorretaId = input.id
+            }
         })
     }
 
