ed988aab57dc316c7273b0a3507f34f12c870bb2
Arthur Menocci <arthur.menocci@eaportal.org>
2024-06-01 03:10:06 +0000

    adicionadas as funções de proxima pergunta e de finalizar oquiz

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -94,7 +94,7 @@ async function buscarPerguntas () {
                     </label>
                 </form>
 
-                <button>Enviar</button>
+                <button>Responder</button>
             </section>
         `
     }
@@ -111,21 +111,42 @@ async function buscarPerguntas () {
         botaoEnviar.addEventListener("click", validarResposta)
     }
 
-        function validarResposta() {
-            if (resposta === quiz.questions[pergunta-1].answer) {
-                document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "correta")
-                pontos = pontos + 1
-            } else {
-                document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "errada")
-                document.querySelector(`label[for='${respostaCorretaId}']`).setAttribute("id", "correta")
-            }
+    function validarResposta() {
+        const botaoEnviar = document.querySelector(".alternativas button") 
+        botaoEnviar.innerText = "Próxima"
+        botaoEnviar.removeEventListener("click", validarResposta) 
+        botaoEnviar.addEventListener("click", proximaPergunta)
+        
+        if (pergunta === 10) {
+            botaoEnviar.innerText = "Finalizar"
+            botaoEnviar.addEventListener("click", finalizar)
+        } else {
+            botaoEnviar.addEventListener("click", proximaPergunta)
         }
 
-    async function iniciar() {
-        alterarAssunto()
-        await buscarPerguntas()
-        montarPergunta()
+        if (resposta === quiz.questions[pergunta-1].answer) {
+            document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "correta")
+            pontos = pontos + 1
+        } else {
+            document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "errada")
+            document.querySelector(`label[for='${respostaCorretaId}']`).setAttribute("id", "correta")
+        }
 
+        pergunta = pergunta + 1
+    }
+
+    function finalizar() {
+        localStorage.setItem("pontos", pontos)
+        
+        window.location.href = "../resultado/resultado.html"
+    }
+
+    function proximaPergunta() {
+        montarPergunta()
+        adicionarEventoInputs()
+    }
+
+    function adicionarEventoInputs() {
         const inputsResposta = document.querySelectorAll(".alternativas input") 
         inputsResposta.forEach (input => {
             input.addEventListener("click", guardarResposta)
@@ -134,6 +155,13 @@ async function buscarPerguntas () {
                 respostaCorretaId = input.id
             }
         })
+    }
+
+    async function iniciar() {
+        alterarAssunto()
+        await buscarPerguntas()
+        montarPergunta()
+        adicionarEventoInputs()
     }
 
     iniciar()
